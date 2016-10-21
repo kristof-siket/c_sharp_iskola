5e1dfb92e3d61b9f75d8e7a947e57143802025c0
Kristof Siket <siketkristof@freemail.hu>
2016-10-21 18:57:01 +0200

    A 'Kezeles' osztály létrehozva. / Class 'Kezeles' (Treatment) created.

---
+++ orvosi_rendelo/orvosi_rendelo/Kezeles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orvosi_rendelo
+{
+    /// <summary>
+    /// A korábbi kezeléseket tároló osztály. / Class of the former treatments.
+    /// </summary>
+    public class Kezeles
+    {
+        public string BNO { get; set; }
+        public string Jegyzet { get; set; }
+    }
+}

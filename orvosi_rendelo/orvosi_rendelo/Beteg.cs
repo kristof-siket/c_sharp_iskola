3ab2b6d8e4ac86bf50cbde3484ac4c6ffa8b399a
Kristof Siket <siketkristof@freemail.hu>
2016-10-21 18:52:51 +0200

    A 'Beteg' osztály létrehozva. / Class 'Beteg' (patient) created.

---
+++ orvosi_rendelo/orvosi_rendelo/Beteg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orvosi_rendelo
+{
+
+    /// <summary>
+    /// A rendelőben kezelt betegek.
+    /// </summary>
+    public class Beteg
+    {
+        public string Nev { get; set; }
+        public string SzHely { get; set;}
+        public DateTime SzDatum { get; set; }
+        public int TajSzam { get; set; }
+        public List<Kezeles> Kezelesek;
+    }
+}

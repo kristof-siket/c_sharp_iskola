570a6731a9ee02850a73ffe6684a60f193bfbb3a
Kristof Siket <siketkristof@freemail.hu>
2016-10-22 13:35:35 +0200

    A ViewModel osztály elkészült. / ViewModel class is ready.

---
+++ orvosi_rendelo/orvosi_rendelo/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orvosi_rendelo
+{
+    public class ViewModel
+    {
+        private static ViewModel _peldany;
+
+        private ViewModel()
+        {
+            Betegek = new ObservableCollection<Beteg>();
+            
+        }
+
+        public static ViewModel Get()
+        {
+            if (_peldany == null)
+            {
+                _peldany = new ViewModel();
+            }
+            return _peldany;
+        }
+
+        public ObservableCollection<Beteg> Betegek { get; set; }
+        public Beteg Kivalasztott { get; set; }
+    }
+}

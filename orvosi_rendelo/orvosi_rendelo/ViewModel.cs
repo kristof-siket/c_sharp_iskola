9d788395d2ecd18a10f1962ddffe13f541a9067a
Kristof Siket <siketkristof@freemail.hu>
2016-10-22 14:22:19 +0200

    ViewModel osztály módosítva. / Class ViewModel modified.

--- orvosi_rendelo/orvosi_rendelo/ViewModel.cs
+++ orvosi_rendelo/orvosi_rendelo/ViewModel.cs
@@ -13,8 +13,23 @@ namespace orvosi_rendelo
 
         private ViewModel()
         {
-            Betegek = new ObservableCollection<Beteg>();
-            
+            Betegek = new ObservableCollection<Beteg>()
+            {
+                new Beteg() { Kezelesek=new List<Kezeles>(), Nev="Siket Kristóf", SzDatum=DateTime.Parse("1996.09.06."), SzHely="Budapest", TajSzam="010101010" },
+                new Beteg() { Kezelesek=new List<Kezeles>(), Nev="Gareth Bale", SzDatum=DateTime.Parse("1986.10.09."), SzHely="Cardiff", TajSzam="020202020" },
+                new Beteg() { Kezelesek=new List<Kezeles>(), Nev="Zámbó Jimmy", SzDatum=DateTime.Parse("1956.05.06."), SzHely="Budapest", TajSzam="435435345" },
+                new Beteg() { Kezelesek=new List<Kezeles>(), Nev="Tupac Amaru Shakur", SzDatum=DateTime.Parse("1970.09.13."), SzHely="Los Angeles", TajSzam="34324234323" },
+                new Beteg() { Kezelesek=new List<Kezeles>(), Nev="Muhammad Ali", SzDatum=DateTime.Parse("1941.11.06."), SzHely="New York", TajSzam="999999999" },
+            };
+
+            Kezelesek = new ObservableCollection<Kezeles>()
+            {
+                new Kezeles() { BNO="BH3829", Jegyzet="Tutira meg fog halni" },
+                new Kezeles() { BNO="HT8343", Jegyzet="Szopogasson strepfen-t, igyon Coldrexet" },
+                new Kezeles() { BNO="TZ7672", Jegyzet="Sebhintőporral, betadinnal kezelni 2 hétig." },
+                new Kezeles() { BNO="KS8327", Jegyzet="Hozzák a koporsót" },
+            };
+
         }
 
         public static ViewModel Get()
@@ -27,6 +42,10 @@ namespace orvosi_rendelo
         }
 
         public ObservableCollection<Beteg> Betegek { get; set; }
-        public Beteg Kivalasztott { get; set; }
+        public Beteg KivalasztottBeteg { get; set; }
+
+        public ObservableCollection<Kezeles> Kezelesek { get; set; }
+        public Kezeles KivalasztottKezeles { get; set; }
+
     }
 }

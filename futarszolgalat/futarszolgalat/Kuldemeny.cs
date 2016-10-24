6fab86b0bf5f16d8abc91b8cfb2facb754a0ea8b
Kristof Siket <siketkristof@freemail.hu>
2016-10-24 21:39:42 +0200

    INotifyPropertyChanged beállítva. / INotifyPropertyChanged has been set.

--- futarszolgalat/futarszolgalat/Kuldemeny.cs
+++ futarszolgalat/futarszolgalat/Kuldemeny.cs
@@ -2,26 +2,36 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace futarszolgalat
-{
-    public class Kuldemeny
+{ 
+    public class Kuldemeny : INotifyPropertyChanged
     {
+        private int gramm;
+
         public string CimzettNev { get; set; }
         public string CimzettOrszag { get; set; }
         public string CimzettVaros { get; set; }
         public string CimzettUtcaHazszam { get; set; }
         public string CimzettIrSzam { get; set; }
-        public int Gramm { get; set; }
+        public int Gramm { get { return gramm; } set { gramm = value; OnPropertyChanged(); } }
         public BindingList<String> Megjegyzesek { get; set; }
         public FutarSzolgaltatas Szolgaltatas { get; set; }
         public int Ar
         {
             get { return Szolgaltatas.Alapar + (Szolgaltatas.GrammAr * Gramm); }
-            set { Ar = value; }
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName]string s = "")
+        {
+            if (PropertyChanged == null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(s));
+            }
+        }
     }
 }

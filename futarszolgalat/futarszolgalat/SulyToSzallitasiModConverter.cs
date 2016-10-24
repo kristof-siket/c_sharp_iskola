f369858484a8685c6c21a59e8b4a0a96b48fb7ce
Kristof Siket <siketkristof@freemail.hu>
2016-10-24 20:58:25 +0200

    A Converter kész. / The Converter is ready.

---
+++ futarszolgalat/futarszolgalat/SulyToSzallitasiModConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace futarszolgalat
+{
+    public class SulyToSzallitasiModConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int suly = (int)value;
+            if (suly < 5000)
+                return "biciklis futár";
+            else if (suly < 50000)
+                return "autós futár";
+            return "teherautó";
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

10de7d96fe37e4646272605c88ec26a250bd2fc2
Kristof Siket <siketkristof@freemail.hu>
2016-10-24 17:54:00 +0200

    'FutarSzolgaltatas' osztály létrehozva. / Class 'FutarSzolgaltatas' (CourierService) created.

---
+++ futarszolgalat/futarszolgalat/FutarSzolgaltatas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace futarszolgalat
+{
+    public class FutarSzolgaltatas
+    {
+        public string Nev { get; set; }
+        public int MinSzallitasiNap { get; set; }
+        public int MaxSzallitasiNap { get; set; }
+        public int Alapar { get; set; }
+        public int GrammAr { get; set; }
+    }
+}

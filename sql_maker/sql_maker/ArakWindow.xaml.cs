90982451e744e7f961120bc6f2a94e777b1a47f6
Kristof Siket <siketkristof@freemail.hu>
2016-10-28 20:51:53 +0200

    ArakWindow kész. / ArakWindow is ready.

---
+++ sql_maker/sql_maker/ArakWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sql_maker
+{
+    /// <summary>
+    /// Interaction logic for ArakWindow.xaml
+    /// </summary>
+    public partial class ArakWindow : Window
+    {
+        ViewModel VM;
+        public ArakWindow()
+        {
+            VM = ViewModel.Get();
+            this.DataContext = VM;
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            VM.Query += string.Format("INSERT INTO Arak values ('{0}', '{1}', '{2}', {3});\n", txtGyarto.Text, txtModell.Text, cmbKateg.SelectedItem.ToString(), txtAr.Text);
+        }
+
+        private void arak_closing_handler(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            VM.FajlbaKiir("add_arak.sql");
+        }
+    }
+}

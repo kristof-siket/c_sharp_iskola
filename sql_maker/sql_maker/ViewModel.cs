cec30d19192338c418df55260ad4f668bb04237c
Kristof Siket <siketkristof@freemail.hu>
2016-10-28 18:18:07 +0200

    ViewModel osztály egyke tervezése kész. / Singleton design of ViewModel is ready.

---
+++ sql_maker/sql_maker/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql_maker
+{
+    public class ViewModel
+    {
+        private static ViewModel _peldany;
+
+        private ViewModel()
+        {
+
+        }
+
+        public static ViewModel Get()
+        {
+            if (_peldany == null)
+            {
+                _peldany = new ViewModel();
+            }
+            return _peldany;
+        }
+
+    }
+}

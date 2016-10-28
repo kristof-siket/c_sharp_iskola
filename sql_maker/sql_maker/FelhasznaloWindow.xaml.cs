f4b74bba44d84503c80c837d4f2cf2ad435050db
Kristof Siket <siketkristof@freemail.hu>
2016-10-28 19:22:40 +0200

    Az ablak majdnem kész. / Window is almost ready (working!).

--- sql_maker/sql_maker/FelhasznaloWindow.xaml.cs
+++ sql_maker/sql_maker/FelhasznaloWindow.xaml.cs
@@ -19,9 +19,25 @@ namespace sql_maker
     /// </summary>
     public partial class FelhasznaloWindow : Window
     {
+        ViewModel VM;
+
         public FelhasznaloWindow()
         {
+            VM = ViewModel.Get();
+            this.DataContext = VM;
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            VM.Query = string.Format("INSERT INTO Felhasznalo values ('{0}', {1}, {2}, {3}, {4}, {5}, {6});\n", txtFelhNev.Text, txtEMail.Text, txtJelszo.Text, txtTeljesNev.Text, txtTelefon.Text, txtLakcim.Text, txtAdmin.Text);
+            txtAdmin.Text = "";
+            txtEMail.Text = "";
+            txtFelhNev.Text = "";
+            txtJelszo.Text = "";
+            txtLakcim.Text = "";
+            txtTelefon.Text = "";
+            txtTeljesNev.Text = "";
         }
     }
 }

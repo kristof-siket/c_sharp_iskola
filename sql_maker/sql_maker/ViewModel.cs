09f2edbfd2ec147be25a0bf971e284dc64bbda28
Kristof Siket <siketkristof@freemail.hu>
2016-10-28 18:38:18 +0200

    A jelenleg használt táblanevek hozzáadva a view model-hez. / The currently used table names has been added to view model.

--- sql_maker/sql_maker/ViewModel.cs
+++ sql_maker/sql_maker/ViewModel.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace sql_maker
 {
+    public enum Tablak { Felhasznalo, Arak, Keszlet, Rendeles} // talán ezzel elegánsabb lenne...
     public class ViewModel
     {
         private static ViewModel _peldany;
 
         private ViewModel()
         {
-
+            OsszesTabla = new ObservableCollection<string>();
+            OsszesTabla.Add("Felhasznalo");
+            OsszesTabla.Add("Arak");
+            OsszesTabla.Add("Keszlet");
+            OsszesTabla.Add("Rendeles");
+            
         }
 
         public static ViewModel Get()
@@ -23,6 +30,9 @@ namespace sql_maker
             }
             return _peldany;
         }
+
+        public ObservableCollection<string> OsszesTabla { get; set; }
+        public string KivalasztottTabla { get; set; }
 
     }
 }

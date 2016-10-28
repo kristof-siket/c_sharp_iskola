ff76744791fd7dad23ed79f18859123175b98861
Kristof Siket <siketkristof@freemail.hu>
2016-10-28 20:29:37 +0200

    Elkészült a működés is. /  It is operating!

--- sql_maker/sql_maker/RendelesWindow.xaml.cs
+++ sql_maker/sql_maker/RendelesWindow.xaml.cs
@@ -19,9 +19,29 @@ namespace sql_maker
     /// </summary>
     public partial class RendelesWindow : Window
     {
+        ViewModel VM;
         public RendelesWindow()
         {
+            VM = ViewModel.Get();
+            DataContext = VM;
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            VM.Query += string.Format("INSERT INTO Rendeles values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}');\n", txtFelhNev.Text, txtGyarto.Text, txtModell.Text, txtSzin.Text, dtpDatum.SelectedDate, txtTeljesitett.Text);
+            
+            txtFelhNev.Text = "";
+            txtGyarto.Text = "";
+            txtModell.Text = "";
+            txtSzin.Text = "";
+            txtTeljesitett.Text = "";
+            dtpDatum.SelectedDate = DateTime.Now;
+        }
+
+        private void rendeles_closing_handler(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            VM.FajlbaKiir("add_rendeles.sql");
         }
     }
 }
